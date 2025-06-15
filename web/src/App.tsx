66e1b0a20e30dda11136e4f4b2dffc07cca7572d
_kitg <gerasimovkit@gmail.com>
2025-06-14 20:02:24 -0400

    chore: web project skeleton

---
+++ web/src/App.tsx
@@ -0,0 +1,16 @@
+import Dashboard from "./pages/Dashboard";
+import LoginPage from "./pages/LoginPage.tsx";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
