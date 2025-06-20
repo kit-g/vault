4957a701f0fb1128a4458519cb7bad00d68ac27d
_kitg <gerasimovkit@gmail.com>
2025-06-20 15:00:52 -0400

    fix: function deployment log silenced

--- api/internal/httpx/version.go
+++ api/internal/httpx/version.go
@@ -1,24 +1,14 @@
 package httpx
 
-import (
-	"os"
-	"runtime"
-)
-
 var (
 	Commit     = "dev"
 	DeployedAt = "unknown"
 )
 
 func Info() map[string]string {
-	hostname, _ := os.Hostname()
 	return map[string]string{
 		"commit":     Commit,
 		"deployedAt": DeployedAt,
-		"goVersion":  runtime.Version(),
-		"goOS":       runtime.GOOS,
-		"goArch":     runtime.GOARCH,
-		"hostname":   hostname,
 	}
 }
 
