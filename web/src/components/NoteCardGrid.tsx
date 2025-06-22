1458def78bef35571ea98ce12737be30276fc158
_kitg <gerasimovkit@gmail.com>
2025-06-22 19:56:28 -0400

    fix: import

--- web/src/components/NoteCardGrid.tsx
+++ web/src/components/NoteCardGrid.tsx
@@ -1,7 +1,7 @@
 import { type NotesResponse } from "../api";
 import { NoteCard } from "./NoteCard.tsx";
 import { useCallback, useEffect, useState } from "react";
-import { Paginator } from "./Paginator.tsx";
+import { Paginator } from "./Paginator";
 
 
 type NoteCardGridProps = {
