ac3adf57c2f9bde7e2bbace94cee8bf5f42b98f6
_kitg <gerasimovkit@gmail.com>
2025-06-24 16:16:36 -0400

    feat: add user management in AuthContext

--- web/src/features/AuthContext.tsx
+++ web/src/features/AuthContext.tsx
@@ -1,16 +1,19 @@
 import { createContext, type ReactNode, useContext, useEffect, useState } from 'react';
+import type { UserOut } from "../api";
 
 interface AuthContextType {
   token: string | null;
   isLoading: boolean;
-  login: (token: string) => void;
+  login: (token: string, user: UserOut) => void;
   logout: () => void;
+  user: UserOut | null;
 }
 
 const AuthContext = createContext<AuthContextType | undefined>(undefined);
 
 export function AuthProvider({ children }: { children: ReactNode }) {
   const [token, setToken] = useState<string | null>(null);
+  // const [user, setUser] = useState<UserOut | null>(null);
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
@@ -21,17 +24,21 @@ export function AuthProvider({ children }: { children: ReactNode }) {
     setIsLoading(false);
   }, []);
 
-  const login = (newToken: string) => {
+  const login = (newToken: string, user: UserOut) => {
     localStorage.setItem("access_token", newToken);
+    localStorage.setItem("user", JSON.stringify(user));
     setToken(newToken);
   };
 
   const logout = () => {
     localStorage.removeItem("access_token");
+    localStorage.removeItem("user");
     setToken(null);
   };
 
-  const value = { token, isLoading, login, logout };
+  const user = JSON.parse(localStorage.getItem("user") || "null") as UserOut | null;
+
+  const value = { token, isLoading, login, logout, user };
 
   return (
     <AuthContext.Provider value={ value }>
